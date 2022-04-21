1abdd665980264d8153524322e5cc0b01847e6f2
Ehsanino82 <ehsan8202@gmail.com>
2022-04-21 18:33:06 +0430

    Login and registration is completed

--- Civ5/src/main/java/Controller/UserController.java
+++ Civ5/src/main/java/Controller/UserController.java
@@ -9,13 +9,51 @@ public class UserController {
     private static ArrayList<User> allUsers = new ArrayList<>();
     private static User loggedInUser;
 
-
-    public static void registerUser(Matcher matcher){
-        //check if register info is correct then add it
+    public static User getLoggedInUser() {
+        return loggedInUser;
     }
 
-    public static void logUserIn(Matcher matcher){
+    public static String registerUser(Matcher matcher){
+        String username = matcher.group("username");
+        String nickname = matcher.group("nickname");
+        String password = matcher.group("password");
+        String output = "";
+        for (User allUser : allUsers) {
+            if (allUser.getUsername().equals(username)) {
+                output = "user with username " + username + " already exists";
+                break;
+            }
+            else if (allUser.getNickname().equals(nickname)) {
+                output = "user with nickname " + nickname + " already exists";
+                break;
+            }
+        }
+        if (output.isEmpty()){
+            output = "user created successfully!";
+            User user = new User(username, password, nickname, false, 0);
+            allUsers.add(user);
+        }
+        return output;
+    }
 
+    public static String logUserIn(Matcher matcher){
+        String username = matcher.group("username");
+        String password = matcher.group("password");
+        String output = "";
+        for (User allUser : allUsers) {
+            if (allUser.getUsername().equals(username) && allUser.getPassword().equals(password)) {
+                output = "user logged in successfully!";
+                loggedInUser = allUser;
+                break;
+            }
+        }
+        if (output.isEmpty()) output = "Username and password didn't match!";
+        return output;
+    }
+
+    public static String logUserOut(){
+        loggedInUser = null;
+        return "user logged out successfully!";
     }
 
 
