3a92850e8405ba1a5f250791608f5c75056cfe4d
Ehsanino82 <ehsan8202@gmail.com>
2022-04-22 15:50:09 +0430

    User and UserController updated

--- Civ5/src/main/java/Controller/UserController.java
+++ Civ5/src/main/java/Controller/UserController.java
@@ -9,11 +9,7 @@ public class UserController {
     private static ArrayList<User> allUsers = new ArrayList<>();
     private static User loggedInUser;
 
-    public static User getLoggedInUser() {
-        return loggedInUser;
-    }
-
-    public static String registerUser(Matcher matcher){
+    public static String registerUser(Matcher matcher) {
         String username = matcher.group("username");
         String nickname = matcher.group("nickname");
         String password = matcher.group("password");
@@ -22,13 +18,12 @@ public class UserController {
             if (allUser.getUsername().equals(username)) {
                 output = "user with username " + username + " already exists";
                 break;
-            }
-            else if (allUser.getNickname().equals(nickname)) {
+            } else if (allUser.getNickname().equals(nickname)) {
                 output = "user with nickname " + nickname + " already exists";
                 break;
             }
         }
-        if (output.isEmpty()){
+        if (output.isEmpty()) {
             output = "user created successfully!";
             User user = new User(username, password, nickname, false, 0);
             allUsers.add(user);
@@ -36,7 +31,7 @@ public class UserController {
         return output;
     }
 
-    public static String logUserIn(Matcher matcher){
+    public static String logUserIn(Matcher matcher) {
         String username = matcher.group("username");
         String password = matcher.group("password");
         String output = "";
@@ -47,14 +42,29 @@ public class UserController {
                 break;
             }
         }
-        if (output.isEmpty()) output = "Username and password didn't match!";
+        if (output.isEmpty()) output = "username and password didn't match!";
         return output;
     }
 
-    public static String logUserOut(){
+    public static String logUserOut() {
         loggedInUser = null;
         return "user logged out successfully!";
     }
 
+    public static ArrayList<User> startGame(ArrayList<String> usernames) {
+        ArrayList<User> players = new ArrayList<>();
+        for (String username : usernames) {
+            if (getUserByUsername(username) == null) return null;
+            else players.add(getUserByUsername(username));
+        }
+        players.add(0, loggedInUser);
+        return players;
+    }
 
+    private static User getUserByUsername(String username) {
+        for (User allUser : allUsers) {
+            if (allUser.getUsername().equals(username)) return allUser;
+        }
+        return null;
+    }
 }
