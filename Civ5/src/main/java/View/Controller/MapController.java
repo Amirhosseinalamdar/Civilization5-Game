13d7538c3a9f191599c6c000f95598fd242a06b0
amirhossein <ah.alamdar546@gmail.com>
2022-07-11 21:51:27 +0430

    tiles FPGC funcs added

--- Civ5/src/main/java/View/Controller/MapController.java
+++ Civ5/src/main/java/View/Controller/MapController.java
@@ -11,17 +11,22 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 
 public class MapController {
     @FXML
     private Pane backgroundPane;
-
+    private int xStartingIndex = 1;
+    private int yStartingIndex = 1;
+    private ArrayList<ImageView> tileImageViews = new ArrayList<>();
+    private ArrayList<ImageView> citizenImageViews = new ArrayList<>();
     public Pane getBackgroundPane() {
         return backgroundPane;
     }
 
-    private int xStartingIndex = 1;
-    private int yStartingIndex = 1;
+
 
     public int getxStartingIndex() {
         return xStartingIndex;
@@ -86,22 +91,22 @@ public class MapController {
                 }
                 if (tile.getResource() != Resource.NONE) {
                     ImageView imageView1 = new ImageView
-                            (new Image("Pictures/resources/"+tile.getResource().toString()+".png"));
+                            (new Image("Pictures/resources/" + tile.getResource().toString() + ".png"));
                     imageView1.setFitWidth(60);
                     imageView1.setFitHeight(60);
                     imageView1.setX(120 * (j - yStartingIndex) + (i % 2) * 60 + 10);
                     imageView1.setY(105 * (i - xStartingIndex) + 10);
                     backgroundPane.getChildren().add(imageView1);
                 }
-                if(tile.canUseItsResource()){
+                if (tile.canUseItsResource()) {
                     ImageView improvementImage = new ImageView(new Image("Pictures/improvments/"
-                            +tile.getResource().getPrerequisiteImprovement().toString()+".png"));
+                            + tile.getResource().getPrerequisiteImprovement().toString() + ".png"));
                     improvementImage.setFitHeight(60);
                     improvementImage.setFitWidth(60);
                     improvementImage.setX(120 * (j - yStartingIndex) + (i % 2) * 60 + 60);
                     improvementImage.setY(105 * (i - xStartingIndex) + 10);
                     backgroundPane.getChildren().add(improvementImage);
-                }else if(tile.getImprovementInProgress() != null){
+                } else if (tile.getImprovementInProgress() != null) {
                     ImageView improvementImage = new ImageView(new Image("Pictures/improvments/inProgress.png"));
                     improvementImage.setFitHeight(20);
                     improvementImage.setFitWidth(20);
@@ -109,7 +114,7 @@ public class MapController {
                     improvementImage.setY(105 * (i - xStartingIndex) + 25);
                     backgroundPane.getChildren().add(improvementImage);
                 }
-                if(tile.isRuined()) {
+                if (tile.isRuined()) {
                     ImageView imageView1 = new ImageView(new Image("Pictures/tiles/ruins.png"));
                     imageView1.setFitWidth(80);
                     imageView1.setFitHeight(80);
@@ -120,5 +125,92 @@ public class MapController {
             }
             flag2 = true;
         }
+
+        if(tileImageViews.size() > 0){//not tested
+            showTilesFoodProductionGold();
+        }
+        if(citizenImageViews.size() > 0){
+            showCitizens();
+        }
+    }
+
+    public void showTilesFoodProductionGold() {//not tested
+        tileImageViews = new ArrayList<>();
+        boolean flag1 = true;
+        boolean flag2 = true;
+        for (int i = xStartingIndex; i < xStartingIndex + 9; i++) {
+            if (i > 0 && flag1) {
+                i = xStartingIndex - 1;
+            }
+            flag1 = false;
+            for (int j = yStartingIndex; j < yStartingIndex + 14; j++) {
+                if (j > 0 && flag2) {
+                    j = yStartingIndex - 1;
+                }
+                flag2 = false;
+                Tile tile = Game.getInstance().getTiles()[i][j];
+
+                if (tile.getFoodPerTurn() > 0) {
+                    String food = "Food" + String.valueOf(tile.getFoodPerTurn()) + ".png";
+                    ImageView IFood = new ImageView(new Image("Pictures/tiles/" + food));
+                    IFood.setX(120 * (j - yStartingIndex) + (i % 2) * 60 + 10);
+                    IFood.setY(105 * (i - xStartingIndex) + 50);
+                    tileImageViews.add(IFood);
+                }
+                if (tile.getGoldPerTurn() > 0) {
+                    String gold = "Gold" + String.valueOf(tile.getGoldPerTurn()) + ".png";
+                    ImageView IGold = new ImageView(new Image("Pictures/tiles/" + gold));
+                    IGold.setX(120 * (j - yStartingIndex) + (i % 2) * 60 + 60);
+                    IGold.setY(105 * (i - xStartingIndex) + 70);
+                    tileImageViews.add(IGold);
+                }
+                if (tile.getProductionPerTurn() > 0) {
+                    String production = "Production" + String.valueOf(tile.getProductionPerTurn()) + ".png";
+                    ImageView IProduction = new ImageView(new Image("Pictures/tiles/" + production));
+                    IProduction.setX(120 * (j - yStartingIndex) + (i % 2) * 60 + 40);
+                    IProduction.setY(105 * (i - xStartingIndex) + 10);
+                    tileImageViews.add(IProduction);
+                }
+            }
+            flag2 = true;
+        }
+        backgroundPane.getChildren().addAll(tileImageViews);
+    }
+
+    public void hideTilesFoodProductionGold(){//not tested
+        backgroundPane.getChildren().removeAll(tileImageViews);
+    }
+
+    public void showCitizens(){
+        citizenImageViews = new ArrayList<>();
+        boolean flag1 = true;
+        boolean flag2 = true;
+        for (int i = xStartingIndex; i < xStartingIndex + 9; i++) {
+            if (i > 0 && flag1) {
+                i = xStartingIndex - 1;
+            }
+            flag1 = false;
+            for (int j = yStartingIndex; j < yStartingIndex + 14; j++) {
+                if (j > 0 && flag2) {
+                    j = yStartingIndex - 1;
+                }
+                flag2 = false;
+                Tile tile = Game.getInstance().getTiles()[i][j];
+                if(tile.getWorkingCitizen() != null) {
+                    ImageView imageView = new ImageView(new Image("Pictures/tiles/Population.png"));
+                    imageView.setFitWidth(30);
+                    imageView.setFitHeight(30);
+                    imageView.setX(120 * (j - yStartingIndex) + (i % 2) * 60 + 40);
+                    imageView.setY(105 * (i - xStartingIndex) + 50);
+                    citizenImageViews.add(imageView);
+                }
+            }
+            flag2 = true;
+        }
+        backgroundPane.getChildren().addAll(citizenImageViews);
+    }
+
+    public void hideCitizen(){
+        backgroundPane.getChildren().removeAll(citizenImageViews);
     }
 }
